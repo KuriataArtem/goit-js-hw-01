5e73999871c3d8c31e0f42ac9fea57a32e325fbe
KuriataArtem <catblindgrandmother@gmail.com>
2025-03-05 08:26:06 +0100

    add //commit

--- js/task1.js
+++ js/task1.js
@@ -1,7 +1,7 @@
 function makeTransaction(quantity, pricePerDroid) {
-    const totalPrice=quantity * pricePerDroid;
-    return `You ordered ${quantity} droids worth ${totalPrice} credits!`
+  const totalPrice = quantity * pricePerDroid;
+  return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
-console.log(makeTransaction(5, 3000));
-console.log(makeTransaction(3, 1000));
-console.log(makeTransaction(10, 500)); 
+console.log(makeTransaction(5, 3000)); //повертає "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000)); //повертає "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 500)); //повертає "You ordered 10 droids worth 5000 credits!"
