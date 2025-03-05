3a8398f7d8e5217f1c9e2624f606ba59c2059e3c
KuriataArtem <catblindgrandmother@gmail.com>
2025-03-05 09:17:11 +0100

    maybe

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,8 @@
 function getElementWidth(content, padding, border) {
-  let contentWidth = parseFloat(content);
-  let paddingWidth = parseFloat(padding);
-  let borderWidth = parseFloat(border);
-  let totalWidth = contentWidth + paddingWidth * 2 + borderWidth * 2;
+  let totalWidth =
+    parseFloat(content) + parseFloat(padding) * 2 + parseFloat(border) * 2;
   return totalWidth;
 }
-console.log(getElementWidth("50px", "8px", "4px")); //returns sum 74
-console.log(getElementWidth("60px", "12px", "8.5px")); //returns sum 101
-console.log(getElementWidth("200px", "0px", "0px")); //returns sum 200
+console.log(getElementWidth('50px', '8px', '4px')); //returns sum 74
+console.log(getElementWidth('60px', '12px', '8.5px')); //returns sum 101
+console.log(getElementWidth('200px', '0px', '0px')); //returns sum 200
