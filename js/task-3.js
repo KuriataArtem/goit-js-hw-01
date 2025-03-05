a83d6971544af11dae2550684820e8fcd2e4a2f2
KuriataArtem <catblindgrandmother@gmail.com>
2025-03-05 09:37:26 +0100

    ./js/

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,8 @@
 function getElementWidth(content, padding, border) {
   let totalWidth =
-    parseFloat(content) + (parseFloat(padding) * 2 + parseFloat(border) * 2);
+    parseFloat(content) + (parseFloat(padding) + parseFloat(border)) * 2;
   return totalWidth;
 }
-console.log(getElementWidth('50px', '8px', '4px')); //returns sum 74
-console.log(getElementWidth('60px', '12px', '8.5px')); //returns sum 101
-console.log(getElementWidth('200px', '0px', '0px')); //returns sum 200
+console.log(getElementWidth('50px', '8px', '4px')); // 74
+console.log(getElementWidth('60px', '12px', '8.5px')); // 101
+console.log(getElementWidth('200px', '0px', '0px')); // 200
